e18ad35f913c52196481f7b65b5eee62b6a5e4ba
fisunens <fisunens@gmail.com>
2024-04-03 14:57:54 +0000

    add kbot.go

---
+++ cmd/kbot.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+// rootCmd represents the base command when called without any subcommands
+var kbotCmd = &cobra.Command{
+	Use:   "kbot",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		fmt.Printf("kbot %s standart", appVersion)
+		kbot.err := telebot.NewBot(telebot.settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+
+			log.Print(m.Message().Payload, m.Text())
+
+			return err
+
+		})
+
+		kbot.Start()
+
+	},
+}
+
+func init() {
+
+	rootCmd.AddCommand(kbotCmd)
+}
